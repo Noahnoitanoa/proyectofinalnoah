979ed3c48d0ccccfccf7feb7e6ee66a18b1f3b0c
Noah Gil <gilpereznoah@gmail.com>
2022-07-23 17:14:01 +0200

    video gif app

--- src/helpers/navbarlist.js
+++ src/helpers/navbarlist.js
@@ -1,4 +1,1 @@
-export const navBarlist = [{id: 1, category: 'packs'},
-                            {id: 2, category: 'maquinaria'},
-                            {id: 3, category: 'lamparas'},
-                            {id: 4, category: 'kits'}];
+export const navBarlist = [{id: 1, category: 'packs'}, {id: 2, category: 'maquinaria'}, {id: 3, category: 'lamparas'}, {id: 4, category: 'kits'}];
